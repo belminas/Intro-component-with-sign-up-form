8b1ed28350f0c432e113587ec8a4fe35f8cc5dee
belminas <trokicmelisa@hotmail.com>
2022-07-20 18:44:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-function introComponent (){
+function introComponent (){ 
+    function validateEmail() 
+        {
+            let email=document.getElementById("email");
+            if (email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))
+            {
+                return (email.classList.remove("is-invalid"));
+                
+            }
+                return (email.classList.add("is-invalid")); 
+        }
+    function validateInput(){
+        let inputs=document.querySelectorAll(".form-control");
+        inputs.forEach(input => {
+            if(input.value=="" || input.value==null){
+                input.classList.add("is-invalid");
+            }
+            else{
+                input.classList.remove("is-invalid");       
+            }  
+        });
+      
+    }  
+function check(event){
+        event.preventDefault(); 
+        validateEmail()
+        validateInput()
+   
+}
     return(
         <div className="row">
             <div className="col">
@@ -14,25 +42,37 @@ function introComponent (){
             </div>
             <div className="card rounded-4" style={{width:"400px"}}>
                 <div className="card-body rounded-4" style={{margin:"3%"}}>
-                <form className="form-floating">
-                <div className="form-floating mb-3">
+                <form  className="form-floating" onSubmit={check} novalidate>
+                <div className="form-floating mb-3 input-container">
                     <input type="text" className="form-control" id="firstName" placeholder="Jonathan"/>
+                    <div id="firstName" className="invalid-feedback">
+                        Email invalid
+                    </div>
                     <label className="lbl" for="firstName">First Name</label>
                 </div>
-                <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="lastName" placeholder="Jonathna"/>
+                <div className="form-floating mb-3 input-container">
+                    <input type="text" className="form-control" id="lastName" placeholder="Jonathna" />
+                    <div id="lastName" className="invalid-feedback">
+                        Please choose a username.
+                    </div>
                     <label className="lbl" for="lastName">Last Name</label>
                 </div>    
-                <div className="form-floating mb-3">
-                    <input type="email" className="form-control" id="email" placeholder="name@example.com"/>
+                <div className="form-floating mb-3 input-container">
+                    <input type="text" className="form-control" id="email" placeholder="name@example.com"/>
+                    <div id="email" className="invalid-feedback">
+                        Email invalid
+                    </div>
                     <label className="lbl" for="emial">Email Address</label>
                 </div>
-                <div className="form-floating mb-3">
+                <div className="form-floating mb-3 input-container">
                     <input type="password" className="form-control" id="password" placeholder="Password"/>
+                    <div id="password" className="invalid-feedback">
+                        Password invalid
+                    </div>
                     <label className="lbl" for="password">Password     </label>  
                 </div>
                 <div className="form-floating mb-3 text-center">
-                    <button type="submit"  className="btn">Claim your free Trial</button>
+                    <button type="submit" className="btn">Claim your free Trial</button>
                 </div>
                 </form>
                 <p><span id="span3">By clicking the button you are agreeing to our</span><span id="span4"> Terms and services</span></p>
@@ -43,5 +83,6 @@ function introComponent (){
     )
 }
  
+
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(introComponent())
